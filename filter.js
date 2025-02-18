adeb674ac4f6f62662d2da3b6a94a73ccb25a47c
Meron Eyeram <35456982+merrhaile@users.noreply.github.com>
2025-02-17 21:25:18 -0500

    Update filter.js
    added reverse string

--- filter.js
+++ filter.js
@@ -1,3 +1,11 @@
 function filterGreater(arr, target) {
   return arr.filter((item) => item > target);
 }
+
+function reverseString(str){
+  temp = "";
+  for(let i = str.length -1; i >= 0; i--){
+    temp += str[i];
+  }
+  return temp;
+}
